a02fb1739e5e03f4d67307a59be70df369ee60a8
abiusx <git@abiusx.com>
2013-07-07 18:25:09 +0430

    ErrorHandler and safe echo variants Change-Id: Id5d2b297571f939260bd0fe982acd94a66cae4f4

---
+++ libs/core/error.php
@@ -0,0 +1,90 @@
+<?php
+namespace phpsec;
+require_once __DIR__."/functions.php";
+class ErrorHandlerAlreadySetException extends \Exception {}
+class ErrorHandlerNotSetException extends \Exception {}
+class ErrorHandler
+{
+	/**
+	 * Holds the previous error_reporting state so that we can get back there
+	 * @var integer
+	 */
+	static private $backupErrorReportingState=null;
+	/**
+	 * Sets the phpsec error handler as error handler
+	 * @throws ErrorHandlerAlreadySetException
+	 */
+	static public function enable()
+	{
+		if (self::isActive())
+			throw new ErrorHandlerAlreadySetException("This function shouldn't be called twice.");
+		self::$backupErrorReportingState=error_reporting();
+		set_error_handler(__NAMESPACE__."\\ErrorHandler::_errorToException");
+		register_shutdown_function(__NAMESPACE__."\\ErrorHandler::_shutdown");
+		error_reporting(0);
+	}
+	/**
+	 * Unsets phpsec error handler, back to the previous one
+	 * @throws ErrorHandlerNotSetException
+	 */
+	static public function disable()
+	{
+		if (!self::isActive())
+			throw new ErrorHandlerNotSetException("This function should be callde after setErrorHandler.");
+		error_reporting(self::$backupErrorReportingState);
+		restore_error_handler ();
+	}
+	
+	/**
+	 * Tells whether or not error handler is active
+	 * @return boolean
+	 */
+	static public function isActive()
+	{
+		return self::$backupErrorReportingState!==null;
+	}
+	/**
+	 * This is registered as a shutdown function to catch fatal errors
+	 * Do not call this directly.
+	 */
+	public static function _shutdown()
+	{
+		$e=error_get_last();
+		if ($e===null) return; //no errors yet!
+		$type=$e['type'];
+
+		//only say fatal error, if the last error has been fatal!
+		if ($type==E_ERROR or $type==E_CORE_ERROR or $type==E_PARSE or $type==E_COMPILE_ERROR or $type==E_USER_ERROR)
+		{
+			if (strpos($e['message'],"ErrorException")===false) //exceptions automatically have filename in their message
+				echof ("Fatal Error ?: ? [?:<strong>?</strong>]",$e['type'],$e['message'],$e['file'],$e['line']);
+			else
+				echo_br("Fatal Error {$e['type']}: {$e['message']}");
+			exit(1);
+		}
+	}
+	
+	/**
+	 * Converts a php error to a php exception (ErrorException)
+	 * You don't need to call this directly.
+	 * @param type $errMsg
+	 * @param type $errNo
+	 * @param type $errFile
+	 * @param type $errLine
+	 * @throws ErrorException
+	 */
+	public static function _errorToException( $errNo ,$errMsg, $errFile = null, $errLine = null,$errContext=null)
+	{
+		throw new \ErrorException($errMsg, $errNo, 0, $errFile, $errLine);
+	}
+
+	/**
+	 * Dumps an exception in readable format
+	 * @param Exception $e
+	 */
+	public static function dump(Exception $e)
+	{
+		echof ($e->getTraceAsString());	
+	}
+	
+}
