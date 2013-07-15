edbec9994dc1ced4f425bb0581bcfddf4482ebd0
Abhishek Das <das.abhshk@gmail.com>
2013-07-16 00:18:06 +0530

    Minor fix: Was using `RequestURI` in place of `Path`; Refs #6
    *   `RequestURI` contains query string by default and hence suitable for
    *   use directly in `ChangeProtocol` method

--- libs/http/http.php
+++ libs/http/http.php
@@ -108,9 +108,9 @@ class HttpRequest extends HttpRequestArray
 		if (self::isCLI())
 			return NULL;
 		if ($QueryString && self::QueryString() )
-			return (self::Protocol()."://".self::ServerName().self::PortReadable().self::RequestURI()."?".self::QueryString());
+			return (self::Protocol()."://".self::ServerName().self::PortReadable().self::Path()."?".self::QueryString());
 		else
-			return (self::Protocol()."://".self::ServerName().self::PortReadable().self::RequestURI());
+			return (self::Protocol()."://".self::ServerName().self::PortReadable().self::Path());
 	}
 
 	/**
@@ -174,9 +174,9 @@ class HttpRequest extends HttpRequestArray
 		if (self::isCLI())
 			return self::URL();
 		if (self::isHTTPS())
-			return (self::PROTOCOL_HTTP."://".self::ServerName().self::PortReadable().self::RequestURI()."?".self::QueryString());
+			return (self::PROTOCOL_HTTP."://".self::ServerName().self::PortReadable().self::RequestURI());
 		else
-			return (self::PROTOCOL_HTTPS."://".self::ServerName().self::PortReadable().self::RequestURI()."?".self::QueryString());
+			return (self::PROTOCOL_HTTPS."://".self::ServerName().self::PortReadable().self::RequestURI());
 	}
 
 	/**
