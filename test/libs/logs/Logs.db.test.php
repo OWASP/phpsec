17d8f1ba71a986da8729de0a4d1cdd5eb9356a4f
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2013-07-25 03:04:30 -0400

    minor change in test case

--- test/libs/logs/Logs.db.test.php
+++ test/libs/logs/Logs.db.test.php
@@ -9,13 +9,21 @@ require_once "../../../libs/logs/logger.php";
 
 class LoggerTest extends \PHPUnit_Framework_TestCase
 {
+	private $myLogger = NULL;
+	
+	public function setUp()
+	{
+		$this->myLogger = new Logger();
+	}
+	
 	public function testCreation()
 	{
 		try
 		{
-			$myLogger = new Logger();
-			$myLogger->log("This is the first message", __FILE__, "WARNING", "LOW");
-			$myLogger->log("This is the second message", __FILE__);
+			$this->myLogger->log("This is the first message", __FILE__, "WARNING", "LOW");
+			$this->myLogger->log("This is the second message", __FILE__);
+			
+			$this->assertTrue(TRUE);
 		}
 		catch (\Exception $e)
 		{
