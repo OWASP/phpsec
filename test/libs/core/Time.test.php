68cf1abf9b4d88277824127df44517eeadd80d48
SvenRtbg <sven@rtbg.de>
2013-07-30 00:33:01 +0200

    Fix some coding style.

--- test/libs/core/Time.test.php
+++ test/libs/core/Time.test.php
@@ -5,38 +5,37 @@ namespace phpsec;
 /**
  * Required Files.
  */
-require_once "../../../libs/core/time.php";
-
+require_once __DIR__ . "/../../../libs/core/time.php";
 
 
 class TimeTest extends \PHPUnit_Framework_TestCase
 {
-	
+
 	/**
 	 * Function to check if correct system time is returned or not.
 	 */
 	public function testSYSTime()
 	{
 		$this->assertSame(\time(), time("SYS"));
-    }
-	
+	}
+
 	/**
 	 * Function to check if current time is returned or not. (2 Cases).
 	 */
 	public function testCurrAndSetTime()
 	{
-        //If user have not set a different time, so the system time must be returned.
+		//If user have not set a different time, so the system time must be returned.
 		$this->assertSame(\time(), time("CURR"));
 
-        // Set arbitrary time value and let it be returned
+		// Set arbitrary time value and let it be returned
 		$this->assertSame(12789, time("SET", 12789));
 
-        // Now CURR should return the set time as current.
-        $this->assertSame(12789, time("CURR"));
+		// Now CURR should return the set time as current.
+		$this->assertSame(12789, time("CURR"));
 
-        sleep(1);
-        // Now CURR should return the set time plus 1 second as current.
-        $this->assertSame(12790, time("CURR"));
+		sleep(1);
+		// Now CURR should return the set time plus 1 second as current.
+		$this->assertSame(12790, time("CURR"));
 
 	}
 
