9019a5246fa6cfcc6f8842027b5b56fb75819a87
SvenRtbg <sven@rtbg.de>
2013-08-02 01:07:57 +0200

    Add readable test configuration for Travis CI database.

--- test/libs/testconfig.php
+++ test/libs/testconfig.php
@@ -6,14 +6,13 @@ namespace phpsec;
  * Required Files
  */
 require_once(__DIR__ . "/../../libs/db/dbmanager.php");
-require_once(__DIR__ . "/../../libs/crypto/confidentialstring.php");
 
 
 try {
-	$username = confidentialString(':/X6NSUlAagxmmLNWRZBA8fyJbmQZmAB7VcgzHHfTxwA=');
-	$password = confidentialString(':bpsY8XdMOZdO32Jnoh7wqh1Og3ogQkIs3e6k8Kvk1J0=');
+	$username = "travis";
+	$password = "";
 
-	DatabaseManager::connect(new DatabaseConfig('pdo_mysql', 'OWASP', $username, $password)); //create a new Db handler.
+	DatabaseManager::connect(new DatabaseConfig('pdo_mysql', 'OWASP', $username, $password, "127.0.0.1")); //create a new Db handler.
 } catch (\Exception $e) {
 	echo $e->getMessage();
 }
