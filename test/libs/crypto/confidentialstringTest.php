77fc7621555530578a533e1a4a7911db546f3bb6
SvenRtbg <sven@rtbg.de>
2013-08-03 00:28:57 +0200

    Fix bigger confidentialstringTest case.

--- test/libs/crypto/confidentialstringTest.php
+++ test/libs/crypto/confidentialstringTest.php
@@ -12,7 +12,6 @@ class ConfidentialStringTest extends \PHPUnit_Framework_TestCase
 	{
 		$this->assertSame(str_pad("root", 32, "\0", STR_PAD_RIGHT), confidentialString(':/X6NSUlAagxmmLNWRZBA8fyJbmQZmAB7VcgzHHfTxwA='));
 		$this->assertSame(str_pad("testing", 32, "\0", STR_PAD_RIGHT), confidentialString(':bpsY8XdMOZdO32Jnoh7wqh1Og3ogQkIs3e6k8Kvk1J0='));
-		$this->assertSame("0123456789012345678901234567890123456789", confidentialString(':7ihOuK5EdTV0+SZYXQp/jPcsxZ0E2xyT86Mf8ykrgy2vtoIGFmKc4EmAqzrXzw9ZcYjUecSyzWLbL5zIGm80cQ=='));
-
+		$this->assertSame(str_pad("0123456789012345678901234567890123456789", 64, "\0", STR_PAD_RIGHT), confidentialString(':7ihOuK5EdTV0+SZYXQp/jPcsxZ0E2xyT86Mf8ykrgy2vtoIGFmKc4EmAqzrXzw9ZcYjUecSyzWLbL5zIGm80cQ=='));
 	}
 }
