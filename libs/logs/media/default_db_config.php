1ca19d3e5eb6386adf509465a568be9e9ec4bd11
SvenRtbg <sven@rtbg.de>
2013-08-03 00:03:00 +0200

    Change loggers default db configuration to work on Travis.

--- libs/logs/media/default_db_config.php
+++ libs/logs/media/default_db_config.php
@@ -11,9 +11,7 @@ return array(
     "MEDIA"	=> "DBLogs",	//Media denotes that the logs must be stored in DB.
     "ADAPTER"	=> "pdo_mysql",	//Adapter denotes the type of DB.
     "DBNAME"	=> "OWASP",	//DBName denotes the name of the DB.
-    "USERNAME"	=> "root",	//Username denotes the username used to access the DB.
-    "PASSWORD"	=> "testing",	//Password denotes the password used to access the DB.
+    "USERNAME"	=> "travis",	//Username denotes the username used to access the DB.
+    "PASSWORD"	=> "",	//Password denotes the password used to access the DB.
     "TABLENAME"	=> "LOGS",	//TableName denotes the name of the table where the logs would be stored.
 );
-
-?>
