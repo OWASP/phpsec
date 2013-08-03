38c3140268985a4ba136a208f25659af3e9205da
SvenRtbg <sven@rtbg.de>
2013-08-03 11:13:30 +0200

    Add some more tests to confidentialstringTest.php

--- test/libs/crypto/confidentialstringTest.php
+++ test/libs/crypto/confidentialstringTest.php
@@ -8,10 +8,25 @@ class ConfidentialStringTest extends \PHPUnit_Framework_TestCase
 	/**
 	 * Function to test the functionality of confidentialString()
 	 */
-	public function testConfidentialString()
+	public function testConfidentialStringIsBasicallyWorking()
 	{
-		$this->assertSame(str_pad("root", 32, "\0", STR_PAD_RIGHT), confidentialString(':/X6NSUlAagxmmLNWRZBA8fyJbmQZmAB7VcgzHHfTxwA='));
-		$this->assertSame(str_pad("testing", 32, "\0", STR_PAD_RIGHT), confidentialString(':bpsY8XdMOZdO32Jnoh7wqh1Og3ogQkIs3e6k8Kvk1J0='));
-		$this->assertSame(str_pad("0123456789012345678901234567890123456789", 64, "\0", STR_PAD_RIGHT), confidentialString(':7ihOuK5EdTV0+SZYXQp/jPcsxZ0E2xyT86Mf8ykrgy2vtoIGFmKc4EmAqzrXzw9ZcYjUecSyzWLbL5zIGm80cQ=='));
+		$this->assertSame("root", confidentialString(':/X6NSUlAagxmmLNWRZBA8fyJbmQZmAB7VcgzHHfTxwA='));
+		$this->assertSame("testing", confidentialString(':bpsY8XdMOZdO32Jnoh7wqh1Og3ogQkIs3e6k8Kvk1J0='));
+		$this->assertSame("0123456789012345678901234567890123456789", confidentialString(':7ihOuK5EdTV0+SZYXQp/jPcsxZ0E2xyT86Mf8ykrgy2vtoIGFmKc4EmAqzrXzw9ZcYjUecSyzWLbL5zIGm80cQ=='));
+	}
+
+	public function testConfidentialStringDoesNotHarmIntentionalZerobytes()
+	{
+		$this->assertSame("\0root", confidentialString(':+25AatjlbzvpXA0RIKW3RwdIEzhiTbxtsfyOD/QQizY='));
+		$this->assertSame("root\0", confidentialString(':/X6NSUlAagxmmLNWRZBA8fyJbmQZmAB7VcgzHHfTxwA='));
+		$this->assertSame("\0root\0", confidentialString(':+25AatjlbzvpXA0RIKW3RwdIEzhiTbxtsfyOD/QQizY='));
+	}
+
+	public function testConfidentialStringDoesNotReplaceVariablesOrFunctionCalls()
+	{
+		$password = str_repeat('a', rand(2, 30));
+		// the next line should be:
+		// $this->assertSame($password, confidentialString($password));
+		$this->assertSame($password, confidentialString(':ddSTcsjF+oR+U0We583AIqVYhCmHulnLurBOiAJCX0A='));
 	}
 }
