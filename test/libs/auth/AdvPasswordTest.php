1ef460694347860aa61fc4d09624878b66332c41
SvenRtbg <sven@rtbg.de>
2013-08-03 12:10:57 +0200

    Removed call to confidentialString - self-modifying test code is really hard, and the values are no secret. Rewritten most tests for ease of use and clarity.

--- test/libs/auth/AdvPasswordTest.php
+++ test/libs/auth/AdvPasswordTest.php
@@ -10,31 +10,28 @@ require_once __DIR__ . "/../../../libs/core/random.php";
 require_once __DIR__ . "/../../../libs/core/time.php";
 require_once __DIR__ . "/../../../libs/auth/user.php";
 require_once __DIR__ . "/../../../libs/auth/adv_password.php";
-require_once (__DIR__ . "/../../../libs/crypto/confidentialstring.php");
 
 
 class AdvPasswordTest extends \PHPUnit_Framework_TestCase
 {
-	protected $userID = "";
-	protected $obj = "";
+	/**
+	 * @var User
+	 */
+	protected $userID;
 
+	/**
+	 * @var AdvancedPasswordManagement
+	 */
+	protected $obj;
 
 	/**
 	 * Function to be run before every test*() functions.
 	 */
 	public function setUp()
 	{
-		try
-		{
-			BasicPasswordManagement::$hashAlgo = "haval256,5";	//choose salting algo.
-			$this->userID = User::newUserObject(randstr(10), confidentialString(':bpsY8XdMOZdO32Jnoh7wqh1Og3ogQkIs3e6k8Kvk1J0='));	//create a user.
-			$this->obj = new AdvancedPasswordManagement($this->userID->getUserID(), confidentialString(':bpsY8XdMOZdO32Jnoh7wqh1Og3ogQkIs3e6k8Kvk1J0='));	//create object to AdvancedPasswordManagement class.
-		}
-		catch (\Exception $e)
-		{
-			echo "\n" . $e->getLine() . "-->";
-			echo $e->getMessage() . "\n";
-		}
+		BasicPasswordManagement::$hashAlgo = "haval256,5"; //choose salting algo.
+		$this->userID = User::newUserObject(randstr(10), 'testing'); //create a user.
+		$this->obj = new AdvancedPasswordManagement($this->userID->getUserID(), 'testing'); //create object to AdvancedPasswordManagement class.
 	}
 
 	/**
@@ -53,26 +50,14 @@ class AdvPasswordTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testCheckIfTempPassExpired()
 	{
-		try {
-			//update the temp pass time to current time.
-			SQL("UPDATE PASSWORD SET TEMP_TIME = ? WHERE USERID = ?", array(time("SYS"), $this->userID->getUserID()));
+		//update the temp pass time to current time.
+		SQL("UPDATE PASSWORD SET TEMP_TIME = ? WHERE USERID = ?", array(time("SYS"), $this->userID->getUserID()));
 
-			$firstTest = $this->obj->checkIfTempPassExpired(); //this check will provide false, since the temp password time has not expired.
+		$this->assertFalse($this->obj->checkIfTempPassExpired()); //this check will provide false, since the temp password time has not expired.
 
-			time("SET", 1390706853); //Now set the time to some distant future time.
+		time("SET", 1390706853); //Now set the time to some distant future time.
 
-			$secondTest = $this->obj->checkIfTempPassExpired(); //this check will provide true, since the temp password time has expired.
-
-			//Make the temp password time as it was.
-			SQL("UPDATE PASSWORD SET TEMP_TIME = ? WHERE USERID = ?", array(0, $this->userID->getUserID()));
-
-			$this->userID->deleteUser();
-
-			$this->assertTrue(!$firstTest && $secondTest);
-		} catch (\Exception $e) {
-			echo "\n" . $e->getLine() . "-->";
-			echo $e->getMessage() . "\n";
-		}
+		$this->assertTrue($this->obj->checkIfTempPassExpired()); //this check will provide true, since the temp password time has expired.
 	}
 
 
@@ -81,55 +66,39 @@ class AdvPasswordTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testTempPassword()
 	{
-		try {
-			$currentTime = time("SYS");
+		$currentTime = time("SYS");
 
-			AdvancedPasswordManagement::$tempPassExpiryTime = 900;
+		AdvancedPasswordManagement::$tempPassExpiryTime = 900;
 
-			$this->obj->tempPassword(); //this will create a new temp password.
+		$this->obj->tempPassword(); //this will create a new temp password.
 
-			$result = SQL("SELECT TEMP_PASS FROM PASSWORD WHERE USERID = ?", array($this->userID->getUserID()));
+		$result = SQL("SELECT TEMP_PASS FROM PASSWORD WHERE USERID = ?", array($this->userID->getUserID()));
 
-			//firstTest
-			time("SET", $currentTime + 500); //set future time that has not passed.
-			$firstTest = $this->obj->tempPassword("qwert"); //This should return false since the password is wrong. Even though time has not expired.
+		//firstTest
+		time("SET", $currentTime + 500); //set future time that has not passed.
+		$this->assertFalse($this->obj->tempPassword("qwert")); //This should return false since the password is wrong. Even though time has not expired.
 
-			//secondTest
-			time("SET", $currentTime + 500);
-			$secondTest = $this->obj->tempPassword($result[0]['TEMP_PASS']); //This should return true since the pasword is correct and time has not expired.
+		//secondTest
+		time("SET", $currentTime + 500);
+		$this->assertTrue($this->obj->tempPassword($result[0]['TEMP_PASS'])); //This should return true since the password is correct and time has not expired.
 
-			//thirdTest
-			time("SET", $currentTime + 1000);
-			$thirdTest = $this->obj->tempPassword($result[0]['TEMP_PASS']); //This should return false since time has expired.
-
-			$this->userID->deleteUser();
-
-			$this->assertTrue(!$firstTest && $secondTest && !$thirdTest);
-		} catch (\Exception $e) {
-			echo "\n" . $e->getLine() . "-->";
-			echo $e->getMessage() . "\n";
-		}
+		//thirdTest
+		time("SET", $currentTime + 1000);
+		$this->assertFalse($this->obj->tempPassword($result[0]['TEMP_PASS'])); //This should return false since time has expired.
 	}
 
 
 	/**
 	 * Function to test if brute force is detected.
+	 *
+	 * @expectedException BruteForceAttackDetectedException
 	 */
 	public function testBruteForce()
 	{
-		try {
-			//repetedly provide wrong password.
-			for ($i = 0; $i < 7; $i++) {
-				$this->obj = new AdvancedPasswordManagement($this->userID->getUserID(), "resting", true); //wrong password provided.
-			}
-
-			//since an exception is generated in the above loop, the below line won't execute.
-			$this->assertTrue(false);
-		} catch (\Exception $e) {
-			$this->userID->deleteUser();
-			$this->assertTrue(true); //If exception is generated, then the function worked.
+		//repeatedly provide wrong password.
+		for ($i = 0; $i < 7; $i++) {
+			$this->obj = new AdvancedPasswordManagement($this->userID->getUserID(), "resting", true); //wrong password provided.
 		}
 	}
-
 
 }
