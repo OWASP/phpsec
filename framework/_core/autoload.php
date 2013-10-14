e9d07b8735e3b28d170eb185b160446d3781c863
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2013-10-14 00:55:56 -0400

    fixed bug that SQL function was not being called because of no DB connection

--- framework/_core/autoload.php
+++ framework/_core/autoload.php
@@ -14,6 +14,8 @@ namespace phpsec\framework;
  *
  */
 require_once Autoloader::phpsecPath()."/core/loader.php";
+require_once (__DIR__ . "/../config/dbconnection.php");
+
 class Autoloader 
 {
 	static function phpsecPath()
---
+++ framework/config/dbconnection.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(__DIR__ . "/../../libs/db/dbmanager.php");
+
+//include necessary files to initiate DB connection and to call the SQL function
+try
+{
+	$config = include(__DIR__ . "/../../libs/config.php");
+    
+        $dbname = $config['DBNAME'];
+        $username = $config['DBUSER'];
+	$password = $config['DBPASS'];
+
+	\phpsec\DatabaseManager::connect(new \phpsec\DatabaseConfig('pdo_mysql', $dbname, $username, $password, "127.0.0.1")); //create a new Db handler.
+} 
+catch (\Exception $e)
+{
+	echo $e->getMessage();
+}
+
+?>
