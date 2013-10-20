1b8124afb87dd6127b132c6e6c55742392eff7d8
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2013-10-19 23:16:23 -0400

    updated config folder in framework

---
+++ framework/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	"USERNAME_SUGGESTION"	=> "ON",
+	"PASSWORD_SUGGESTION"	=> "ON",
+	"BRUTE_FORCE_DETECTION"	=> "ON",
+);
+
+?>
--- framework/config/routes.php
+++ framework/config/routes.php
@@ -8,4 +8,13 @@ namespace phpsec\framework;
  * @note: wildcards only supported at the rightmost character
  */
 
-FrontController::$Routes["*"]="default"; //route everything else to default
+FrontController::$Routes["home"] =			"general/indexcontroller";			//the first page for users (non-logged users)
+FrontController::$Routes["login"] =			"users/userslogincontroller";			//for user login
+FrontController::$Routes["logout"] =			"users/userlogoutcontroller";			//for user logout
+FrontController::$Routes["user/index"] =			"users/userindexcontroller";			//the first page for users
+FrontController::$Routes["signup"] =			"users/usersignupcontroller";			//for user signup
+FrontController::$Routes["forgotpassword"] =		"users/forgotpasswordcontroller";		//for user forgot password
+FrontController::$Routes["requestnewpassword"] =		"users/requestnewpasswordcontroller";		//for user setting new password after forgot password
+FrontController::$Routes["temppass"] =			"users/temppasscontroller";			//for account activation and temp password management
+FrontController::$Routes["passwordreset"] =		"users/passwordresetcontroller";		//for password reset
+FrontController::$Routes["*"] =				"default";					//route everything else to default
