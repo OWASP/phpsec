4657ce554232c5ce9e828fb895e016ad0f73b70b
Abhishek Das <das.abhshk@gmail.com>
2014-08-18 23:25:45 +0530

    Creates wrapper around captcha library

---
+++ libs/form/captcha/config.sample.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace phpsec;
+
+return array(
+
+    /**
+     * Get a key from https://www.google.com/recaptcha/admin/create
+     *
+     */
+    "captcha_public_key" => "",
+    "captcha_private_key" => "",
+);
+
+?>
